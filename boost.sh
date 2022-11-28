64d3c1e79c8820f1c5863c38ed34b344483be3d2
hank <hank@henrygd.me>
2022-11-28 10:51:04 -0600

    initial commit

---
+++ boost.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CUR_USER="$(whoami)"
+
+PS3="Choose action: "
+
+select lng in "Start site" "Stop Site" "Create site" "Restart Site" "Fix permissions" "Quit"
+do
+    case $lng in
+        "Start site")
+          echo -e "\e[36mStarting site...\e[0m"
+          read -p "Enter site name or abbreviation (no spaces): " sitename
+           docker compose -f "/home/$CUR_USER/sites/$sitename/docker-compose.yml" up -d
+          break;;
+        "Stop Site")
+          echo -e "\e[36mStopping site...\e[0m"
+          read -p "Enter site name or abbreviation (no spaces): " sitename
+          docker compose -f "/home/$CUR_USER/sites/$sitename/docker-compose.yml" stop
+          break;;
+        "Restart Site")
+          echo -e "\e[36mRestarting site...\e[0m"
+          read -p "Enter site name or abbreviation (no spaces): " sitename
+          docker compose -f "/home/$CUR_USER/sites/$sitename/docker-compose.yml" restart
+          break;;
+        "Create site")
+          echo -e "\e[36mCreating site...\e[0m"
+          curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/newsite.sh > ./newsite.sh && ./newsite.sh
+          break;;
+        "Fix permissions")
+          echo -e "\e[36mFixing permissions...\e[0m"
+          read -p "Enter site name or abbreviation (no spaces): " sitename
+          sudo chown -R nobody: "/home/$CUR_USER/sites/$sitename"
+          break;;
+        "Quit")
+          echo "Goodbye :)"
+          break;;
+        *)
+          echo "huh?";;
+    esac
+  rm ./.boost.sh
+done
---
+++ newsite.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+CUR_USER="$(whoami)"
+
+read -p "Enter site name or abbreviation (no spaces): " sitename
+
+if [ -d "/home/$CUR_USER/sites/$sitename" ]
+then
+  echo -e "\e[31mFolder exists. Exiting..."
+  exit;
+fi
+
+mkdir -p "/home/$CUR_USER/sites/$sitename/wordpress"
+curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/wordpress/docker-compose.yml > "/home/$CUR_USER/sites/$sitename/docker-compose.yml"
+curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/wordpress/.htninja > "/home/$CUR_USER/sites/$sitename/.htninja"
+chown nobody: /home/$CUR_USER/sites/$sitename/.htninja
+
+# replace yml with site name
+sed -i "s/CHANGE_TO_SITE_NAME/$sitename/" "/home/$CUR_USER/sites/$sitename/docker-compose.yml"
+sed -i "s/CHANGE_TO_USERNAME/$sitename/" "/home/$CUR_USER/sites/$sitename/docker-compose.yml"
+
+echo -e "\e[32mSite created at /home/$CUR_USER/sites/$sitename/wordpress\e[0m"
+
+read -p "Start site now and create a fresh wp installation (y/n)? " -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  docker compose -f "/home/$CUR_USER/sites/$sitename/docker-compose.yml" create
+  echo "Upload your files and start the site later. Goodbye :)"
+  rm ./newsite.sh
+  exit;
+fi
+
+# start site
+docker compose -f "/home/$CUR_USER/sites/$sitename/docker-compose.yml" up -d
+rm ./newsite.sh
