abf3bb0050b678ba4df4b1566de5cccc94512a4e
hank <hank@henrygd.me>
2023-01-21 12:25:29 -0600

    add .htninja back

--- newsite.sh
+++ newsite.sh
@@ -12,6 +12,7 @@ fi
 
 mkdir -p "/home/$CUR_USER/sites/$sitename/wordpress"
 curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/wordpress/docker-compose.yml > "/home/$CUR_USER/sites/$sitename/docker-compose.yml"
+curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/wordpress/.htninja > "/home/$CUR_USER/sites/$sitename/.htninja"
 
 read -r -p 'Type "YES" if this site requires PHP 7: ' oldphp
 
