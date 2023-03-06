96257d99a5e45fa6db64edfc8e1ff48525befa84
hank <hank@henrygd.me>
2023-03-06 09:26:46 -0600

    rm name ctop for ctop container to allow multiple instances

--- setup.sh
+++ setup.sh
@@ -185,7 +185,7 @@ done
   echo 'alias dcu="docker compose up -d"';
   echo 'alias dcr="docker compose restart"';
   echo 'alias boost="curl -s https://raw.githubusercontent.com/BOOST-Creative/docker-server-setup/main/boost.sh > ~/.boost.sh && chmod +x ~/.boost.sh && ~/.boost.sh"';
-  echo 'alias ctop="docker run --rm -ti --name=ctop --volume /var/run/docker.sock:/var/run/docker.sock:ro quay.io/vektorlab/ctop:latest"';
+  echo 'alias ctop="docker run --rm -ti --volume /var/run/docker.sock:/var/run/docker.sock:ro quay.io/vektorlab/ctop:latest"';
   echo 'echo -e "\nPortainer: \e[34mhttp://localhost:6900\n\e[0mNginx Proxy Manager: \e[34mhttp://localhost:6901\n\e[0mphpMyAdmin: \e[34mhttp://localhost:6902\n\e[0mFile Browser: \e[34mhttp://localhost:6903\n\e[0mKopia: \e[34mhttp://localhost:6904\e[0m (kopia:'"$KOPIA_PASSWORD"')\nDozzle: \e[34mhttp://localhost:6905\n\n\e[0mRun ctop to manage containers and view metrics.\n"';
   echo 'type ~/firewall.sh &>/dev/null && ./firewall.sh';
 } >> "/home/$username/.bashrc"
